0bf38d6ee38a62f835f4c35ef8387ff3175fafca
Larry N <larryn35@gmail.com>
2020-09-23 18:16:36 -0700

    initial commit

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Larry Nguyen on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
