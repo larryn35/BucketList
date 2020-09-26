155a80b94364ea88f293d3d107aa1550c1cdf05d
Larry N <larryn35@gmail.com>
2020-09-25 20:38:07 -0700

    add title and subtitle to pin

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var locations = [MKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
+    @State private var showingEditScreen = false
     
     var body: some View {
         ZStack {
@@ -32,6 +33,8 @@ struct ContentView: View {
                         newLocation.coordinate = self.centerCoordinate
                         newLocation.title = "Example location"
                         self.locations.append(newLocation)
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
                     }) {
                         Image(systemName: "plus")
                     }
@@ -46,8 +49,14 @@ struct ContentView: View {
         }
         .alert(isPresented: $showingPlaceDetails) {
             Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place info"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                    // edit place
+                // edit place
+                self.showingEditScreen = true
             })
+        }
+        .sheet(isPresented: $showingEditScreen) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
         }
         
     }
